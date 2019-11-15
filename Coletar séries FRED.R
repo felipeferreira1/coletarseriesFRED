3b28145a73d880c2044c6b889cfcb95a96838689
Felipe Simplício Ferreira <felipe.ferreira@ipea.gov.br>
2019-11-15 14:53:22 -0300

    Mudança de nome e correção de bug
    Mudança de nome da função para evitar conflito com outras funções que fiz (coletar SGS) e correção de bug no print do progresso da repetição, que não estava pegando o tamanho do argumento certo

--- Coletar séries FRED.R
+++ Coletar séries FRED.R
@@ -12,7 +12,7 @@ setwd("C:/Users/User/Documents")
 library("fredr")
 
 #Criando função para coleta de séries
-coleta_dados = function(series, datainicial="2018-01-01", datafinal = format(Sys.time(), "%Y-%m-%d")){
+coleta_dados_fred = function(series, datainicial="2018-01-01", datafinal = format(Sys.time(), "%Y-%m-%d")){
   #Argumentos: vetor de séries, datainicial que pode ser manualmente alterada e datafinal que automaticamente usa a data de hoje
   #Chave para funcionamento da API do FRED
   fredr_set_key("759fd9f905b9d4025ce26e5ae6e63cb9")
@@ -28,7 +28,7 @@ coleta_dados = function(series, datainicial="2018-01-01", datafinal = format(Sys
       base = dados1[,-2] #Primeira repetição cria o dataframe
     else
       base = merge(base, dados[,-2], by = "data", all = T) #Demais repetições agregam colunas ao dataframe criado
-    print(paste(i, length(serie), sep = '/')) #Printa o progresso da repetição
+    print(paste(i, length(series), sep = '/')) #Printa o progresso da repetição
   }
   
   base$data = as.Date(base$data, "%d/%m/%Y") #Transforma coluna de data no formato de data
@@ -40,6 +40,6 @@ coleta_dados = function(series, datainicial="2018-01-01", datafinal = format(Sys
 #Definida a função, podemos criar objetos para guardar os resultados
 #Vetor com código para várias séries do FRED
 serie = c("ALTSALES", "AWHAETP", "CES0500000003", "CES0500000011", "CIVPART", "CPIAUCSL", "CPILFESL", "CUSR0000SEHA", "CUSR0000SEHC", "EXHOSLUSM495S", "GS10", "HOUST", "HSN1F", "INDPRO", "IPMANSICS", "IPMINE", "IPUTIL", "PAYEMS", "PCEPI", "PCEPILFE", "PERMIT", "RSAFS", "RSXFS", "SPCS20RNSA", "SPCS20RSA", "TTLCONS", "TWEXMMTH", "TWEXMPA", "U6RATE", "UNRATE")
-ex = coleta_dados(serie) #Criando objeto em que ficam guardados as séries
+ex = coleta_dados_fred(serie) #Criando objeto em que ficam guardados as séries
 
 write.csv2(ex, "exemplo.csv", row.names = F) #Salvando arquivo csv em padrão brasileiro
